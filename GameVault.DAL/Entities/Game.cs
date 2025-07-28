506e998b5ab332ac27d7d2a2b048bb4d3628e962
bassam348 <162562515+bassam348@users.noreply.github.com>
2025-07-28 15:46:27 +0200

    Update Game.cs

--- GameVault.DAL/Entities/Game.cs
+++ GameVault.DAL/Entities/Game.cs
@@ -3,5 +3,18 @@ namespace GameVault.DAL.Entites
 {
     public class Game
     {
+        public int Game_Code { get; private set; }
+        public string title { get; private set; }
+        public List<Category>? Categorys { get; set; } = new List<Category>();
+        public List<Review>? Reviews { get; set; } = new List<Review>();
+        public List<User>? Users { get; set; } = new List<User>();
+
+        [ForeignKey("Company")]
+        public int? Company_ID { get; private set; };
+        public Company? Company { get; private set; }
+        [ForeignKey("Category")]
+        public int? Category_ID { get; private set; };
+        public Category? Category { get; private set; }
+
     }
 }
