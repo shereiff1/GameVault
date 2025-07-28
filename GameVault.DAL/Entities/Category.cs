db149400a6c873a95dc6925a488676a6f1904277
bassam348 <162562515+bassam348@users.noreply.github.com>
2025-07-28 16:26:02 +0200

    Update Category.cs

--- GameVault.DAL/Entities/Category.cs
+++ GameVault.DAL/Entities/Category.cs
@@ -7,5 +7,11 @@ namespace GameVault.DAL.Entities
         public int Category_ID;
         public string Category_Name;
         public string Description;
+         public DateTime? CreatedOn { get; private set; }
+         public bool IsDeleted { get; private set; }
+         public DateTime? ModifiedOn { get; private set; }
+         public string CreatedBy { get; private set; }
+
+        public List<Game>? Games { get; set; } = new List<Game>();
     }
 }
