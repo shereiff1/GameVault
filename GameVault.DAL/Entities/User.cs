b33949f2e6925ce766c70600752f835c08a1e3bf
bassam348 <162562515+bassam348@users.noreply.github.com>
2025-07-28 15:56:23 +0200

    Update User.cs

--- GameVault.DAL/Entities/User.cs
+++ GameVault.DAL/Entities/User.cs
@@ -3,5 +3,7 @@ namespace GameVault.DAL.Entities
 {
     public class User
     {
+        public int User_ID {get; private set;}
+        public List<Game>? Games {get; set;} = new List<Game>();
     }
 }
