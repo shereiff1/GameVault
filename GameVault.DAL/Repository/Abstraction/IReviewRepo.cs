c763a1538337f69bbac3a0cb3727cf858af01a48
bassam348 <162562515+bassam348@users.noreply.github.com>
2025-07-31 13:40:13 +0200

    Update IReviewRepo.cs

--- GameVault.DAL/Repository/Abstraction/IReviewRepo.cs
+++ GameVault.DAL/Repository/Abstraction/IReviewRepo.cs
@@ -1,8 +1,12 @@
-﻿
+using GameVault_DAL.Entities;
 
-namespace GameVault.DAL.Repository.Abstraction
+namespace GameVault_DAL.Repo.Abstraction
 {
     public interface IReviewRepo
     {
+        (bool, string?) Create(Review Dept);
+        List<Review> GetAll();
+        (bool, string?) Delete(int id);
+        (bool, string?) Update(Review Dept);
     }
 }
