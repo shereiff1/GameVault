6c05bad82c5e13868571d871df3c98a42486dd65
bassam348 <162562515+bassam348@users.noreply.github.com>
2025-07-31 13:39:05 +0200

    Update ICategoryRepo.cs

--- GameVault.DAL/Repository/Abstraction/ICategoryRepo.cs
+++ GameVault.DAL/Repository/Abstraction/ICategoryRepo.cs
@@ -1,8 +1,12 @@
-﻿
+using GameVault_DAL.Entities;
 
-namespace GameVault.DAL.Repository.Abstraction
+namespace GameVault_DAL.Repo.Abstraction
 {
     public interface ICategoryRepo
     {
+        (bool, string?) Create(Category Dept);
+        List<Category> GetAll();
+        (bool, string?) Delete(int id);
+        (bool, string?) Update(Category Dept);
     }
 }
