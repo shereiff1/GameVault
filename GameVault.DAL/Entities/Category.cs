1289301abda5b2b6f73be1a23b8daacce5c90397
shereiff1 <147086400+shereiff1@users.noreply.github.com>
2025-08-01 19:35:59 +0300

    Your changes to Category.cs

--- GameVault.DAL/Entities/Category.cs
+++ GameVault.DAL/Entities/Category.cs
@@ -1,19 +1,57 @@
-﻿
+﻿using GameVault.DAL.Entites;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
 
-using GameVault.DAL.Entites;
-
-namespace GameVault.DAL.Entities
+namespace GameVault_DAL.Entities
 {
     public class Category
     {
-        public int CategoryID;
-        public string CategoryName;
-        public string Description;
-        public DateTime? CreatedOn { get; private set; }
+        [Required]
+        public int Category_Id { get; private set; }
+        public string Category_Name { get; private set; }
+        public string Description { get; private set; }
+        public DateTime CreatedOn { get; private set; }
         public bool IsDeleted { get; private set; }
-        public DateTime? ModifiedOn { get; private set; }
+        public DateTime ModifiedOn { get; private set; }
         public string CreatedBy { get; private set; }
+        public List<Game>? Gamess { get; set; } = new List<Game>();
+        public Category()
+        {
+            CreatedOn = DateTime.Now;
+            IsDeleted = false;
+            ModifiedOn = DateTime.Now;
+        }
+        public Category(string name, string discription, string createdBy = "Admin")
+        {
+            Category_Name = name;
+            Description = discription;
+            CreatedOn = DateTime.Now;
+            IsDeleted = false;
+            ModifiedOn = DateTime.Now;
+            CreatedBy = createdBy;
+        }
+        public Category(int id, string name, string discription, string createdBy = "Admin")
+        {
+            Category_Id = id;
+            Category_Name = name;
+            Description = discription;
+            CreatedOn = DateTime.Now;
+            IsDeleted = false;
+            ModifiedOn = DateTime.Now;
+            CreatedBy = createdBy;
+        }
+        public void DELETE()
+        {
+            IsDeleted = true;
+            ModifiedOn = DateTime.Now;
+        }
+        public void Update(int id, string name, string discription)
+        {
+            Category_Id = id;
+            Category_Name = name;
+            Description = discription;
+            ModifiedOn = DateTime.Now;
+        }
 
-        public List<Game>? Games { get; set; } = new List<Game>();
     }
 }
