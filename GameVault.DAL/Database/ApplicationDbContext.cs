229c9c024cf1f01270e433c9568b2928dcb2043b
shereiff1 <147086400+shereiff1@users.noreply.github.com>
2025-08-01 23:51:52 +0300

    Update ApplicationDbContext.cs
    Identity DB

--- GameVault.DAL/Database/ApplicationDbContext.cs
+++ GameVault.DAL/Database/ApplicationDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace GameVault.DAL.Database
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
