5b1674584144550a5e3309e46406101e6009f823
shereiff1 <147086400+shereiff1@users.noreply.github.com>
2025-08-01 23:56:03 +0300

    Create UserPublicProfile.cs

---
+++ GameVault.BLL/ModelVM/User/UserPublicProfile.cs
@@ -0,0 +1,10 @@
+
+namespace GameVault.BLL.ModelVM.User
+{
+    public class UserPublicProfile
+    {
+        public string? Name { get; set; }
+        public string? UserName { get; set; }
+        public string? ProfilePicture { get; private set; }
+    }
+}
