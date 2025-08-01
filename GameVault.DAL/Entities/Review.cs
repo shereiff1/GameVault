1e8bdef1593927e48c1baa5478648a5018f08273
bassam348 <162562515+bassam348@users.noreply.github.com>
2025-08-01 21:28:09 +0200

    Update Review.cs

--- GameVault.DAL/Entities/Review.cs
+++ GameVault.DAL/Entities/Review.cs
@@ -1,36 +1,60 @@
-﻿
+using System.ComponentModel.DataAnnotations;
 
-using System.ComponentModel.DataAnnotations.Schema;
-using GameVault.DAL.Entites;
-
-namespace GameVault.DAL.Entities
+namespace GameVault_DAL.Entities
 {
     public class Review
     {
-        public Review(string comment, float rating, int userId, string createdBy, int gameId)
+        [Key]
+        public int Review_Id { get; private set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Player_Id must be a positive integer.")]
+        public int Player_Id { get; private set; }
+
+        public string Comment { get; private set; }
+
+        [Required]
+        [Range(0, 5, ErrorMessage = "Rating must be between 0 and 5.")]
+        public float Rating { get; private set; }
+        public DateTime CreatedOn { get; private set; }
+        public bool IsDeleted { get; private set; }
+        public DateTime? ModifiedOn { get; private set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string CreatedBy { get; private set; }
+        [ForeignKey(Game)]
+        public int Game_Id { get; private set; }
+        public virtual Game? Game { get; private set; } 
+        public Review()
         {
+            CreatedOn = DateTime.Now;
+            IsDeleted = false;
+            ModifiedOn = DateTime.Now;
+        }
+        public Review(int player_id, string comment, float rating, int game_id, string createdBy = "Admin")
+        {
+            Player_Id = player_id;
             Comment = comment;
             Rating = rating;
-            UserId = userId;
+            Game_Id = game_id;
             CreatedOn = DateTime.Now;
-            CreatedBy = createdBy;
-            GameId = gameId;
             IsDeleted = false;
+            ModifiedOn = DateTime.Now;
+            CreatedBy = createdBy;
         }
-
-        public int ReviewId { get;  set; }
-        public string Comment { get;  set; }
-        public float Rating { get;  set; }
-        [ForeignKey("User")]
-        public int UserId { get;  set; }
-        public DateTime? CreatedOn { get;  set; }
-        public bool IsDeleted { get;  set; }
-        public DateTime? ModifiedOn { get;  set; }
-        public string CreatedBy { get;  set; }
-        [ForeignKey("Game")]
-        public int GameId { get; set; }
-
-
-
+        public void DELETE()
+        {
+            IsDeleted = true;
+            ModifiedOn = DateTime.Now;
+        }
+        public void Update(int review_id, int player_id , string comment, float rating)
+        {
+            Review_Id= review_id;
+            Player_Id = player_id;
+            Comment = comment;
+            Rating = rating;
+            ModifiedOn = DateTime.Now;
+        }
     }
 }
