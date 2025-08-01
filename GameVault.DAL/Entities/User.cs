de5dce6dbec8c5576fe529a306d9c17a8a63847b
shereiff1 <147086400+shereiff1@users.noreply.github.com>
2025-08-01 23:50:51 +0300

    Update User.cs

--- GameVault.DAL/Entities/User.cs
+++ GameVault.DAL/Entities/User.cs
@@ -1,11 +1,81 @@
-﻿
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using GameVault.DAL.Entites;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GameVault.DAL.Entities
 {
-    public class User
+    public class User : IdentityUser
     {
-        public int UserId {get; private set;}
-        public List<Game>? Games {get; set;} = new List<Game>();
+        [MaxLength(150)]
+        public string? Name { get; private set; }
+        public DateTime CreatedOn { get; private set; }
+        public DateTime? ModifiedOn { get; private set; }
+        public bool? IsDeleted { get; private set; }
+        public DateTime? DeletedOn { get; private set; }
+        public string? ProfilePicture { get; private set; }
+        public List<Game>? Library {get; private set;} = new List<Game>();
+
+        //public List<User>? Friends { get; private set; } = new List<User>();
+
+        public User(string Email, string Username, string Password) : base()
+        {
+            this.Email = Email;
+            this.UserName = Username;
+            this.PasswordHash = Password;
+            
+        }
+
+        public void UpdateProfile(string? Username, string? Name, string? ImagePath)
+        {
+            if(Name != null)
+                this.Name = Name;
+            if(UserName != null)
+                this.UserName = Username;
+            this.ModifiedOn = DateTime.Now;
+            if(ImagePath != null)
+                this.ProfilePicture = ImagePath;
+        }
+        public void DeleteUser()
+        {
+            this.IsDeleted = true;
+            this.DeletedOn = DateTime.Now;
+        }
+
+        public void AddGameToLibrary(Game game)
+        {
+            if (game == null) 
+                throw new ArgumentNullException(nameof(game));
+            if (Library == null) 
+                Library = new List<Game>();
+            Library.Add(game);
+        }   
+
+        public void RemoveGameFromLibrary(Game game)
+        {
+            if (game == null) 
+                throw new ArgumentNullException(nameof(game));
+            if (Library == null) 
+                return;
+            Library.Remove(game);
+        }
+        //public void AddFriend(User friend)
+        //{
+        //    if (friend == null) 
+        //        throw new ArgumentNullException(nameof(friend));
+        //    if (Friends == null) 
+        //        Friends = new List<User>();
+        //    Friends.Add(friend);
+        //}
+        //public void RemoveFriend(User friend)
+        //{
+        //    if (friend == null)
+        //        throw new ArgumentNullException(nameof(friend));
+        //    if (Friends == null)
+        //        return;
+        //    Friends.Remove(friend);
+        //}
+
     }
 }
