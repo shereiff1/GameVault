7559d6c5da2c4707b55a85684f1a93982ae87b29
shereiff1 <147086400+shereiff1@users.noreply.github.com>
2025-08-01 23:59:12 +0300

    Create GameVM.cs

---
+++ GameVault.BLL/ModelVM/Game/GameVM.cs
@@ -0,0 +1,11 @@
+
+namespace GameVault.BLL.ModelVM
+{
+    public class GameVM
+    {
+        // the ctor string title int companyId, string createdBy
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
