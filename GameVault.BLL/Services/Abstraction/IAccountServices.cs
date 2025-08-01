b52a5d073e851d292e4753960a0b240fada7ecac
shereiff1 <147086400+shereiff1@users.noreply.github.com>
2025-08-02 00:07:47 +0300

    Create IAccountServices.cs

---
+++ GameVault.BLL/Services/Abstraction/IAccountServices.cs
@@ -0,0 +1,18 @@
+
+
+using GameVault.BLL.ModelVM.User;
+using GameVault.DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace GameVault.BLL.Services.Abstraction
+{
+    public interface IAccountServices
+    {
+         Task<IdentityResult> Login(UserLogin user);
+
+         Task<IdentityResult> SignUp(UserSignUp user);
+
+        void Logout();
+
+    }
+}
