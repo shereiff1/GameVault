1079e51696ca95e956e508becd464af113303e3a
bassam348 <162562515+bassam348@users.noreply.github.com>
2025-08-01 21:28:57 +0200

    Update ICategoryRepo.cs

--- GameVault.DAL/Repository/Abstraction/ICategoryRepo.cs
+++ GameVault.DAL/Repository/Abstraction/ICategoryRepo.cs
@@ -1,8 +1,12 @@
-﻿
+using GameVault_DAL.Entities;
 
-namespace GameVault.DAL.Repository.Abstraction
+namespace GameVault_DAL.Repo.Abstraction
 {
     public interface ICategoryRepo
     {
+        Task<(bool, string?)> CreateAsync(Category category);
+        Task<List<Category>> GetAllAsync();
+        Task<(bool, string?)> DeleteAsync(int id);
+        Task<(bool, string?)> UpdateAsync(Category category);
     }
 }
