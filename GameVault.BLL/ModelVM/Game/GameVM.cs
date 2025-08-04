24d65dc8591db03f8c0dc0c8a0aa3d54f3394ed6
bassam348 <162562515+bassam348@users.noreply.github.com>
2025-08-04 22:12:17 +0200

    adding file

--- GameVault.BLL/ModelVM/Game/GameVM.cs
+++ GameVault.BLL/ModelVM/Game/GameVM.cs
@@ -7,5 +7,6 @@ namespace GameVault.BLL.ModelVM
         public int Id { get; set; }
         public string Title { get; set; }
         public string CreatedBy { get; set; }
+        public IFormFile? File { get; set; }
     }
 }
