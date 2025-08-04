1f8120ad28eb6fb676c5cd9fd81a1d18da8ef9c8
shereiff1 <147086400+shereiff1@users.noreply.github.com>
2025-08-05 00:55:00 +0300

    Create GameDTO.cs
    Adding Data Transfer Object

---
+++ GameVault.DAL/Entities/GameDTO.cs
@@ -0,0 +1,17 @@
+
+
+namespace GameVault.DAL.Entities
+{
+    public class GameDTO
+    {
+        public int GameId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string CompanyName { get; set; }
+        public List<string>? Reviews { get; set; }
+        public List<string>? Categories { get; set; }
+        public float Rating { get; set; }
+        public string? ImagePath { get; set; }
+        public decimal Price { get; set; }
+    }
+}
