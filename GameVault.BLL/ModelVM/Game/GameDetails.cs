9bbd796c0fb5825a9d1ad25d54e71a1e608aa012
shereiff1 <147086400+shereiff1@users.noreply.github.com>
2025-08-05 00:59:43 +0300

    Create GameDetails.cs
    Adding Game Details

---
+++ GameVault.BLL/ModelVM/Game/GameDetails.cs
@@ -0,0 +1,17 @@
+using GameVault.DAL.Entities;
+
+namespace GameVault.BLL.ModelVM.Game
+{
+    public class GameDetails
+    {
+        public int GameId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string CompanyName { get; set; }
+        public  List<string>? Reviews { get;  set; } 
+        public  List<string>? Categories { get;  set; } 
+        public float Rating { get; set; }
+        public string? ImagePath { get; set; }
+        public decimal Price { get; set; }
+    }
+}
