42d685b8a6f220521c0789cc4827b89f9b2a1760
bassam348 <162562515+bassam348@users.noreply.github.com>
2025-08-06 17:03:40 +0200

    Update Upload.cs

--- GameVault.BLL/Helpers/UploadFile/Upload.cs
+++ GameVault.BLL/Helpers/UploadFile/Upload.cs
@@ -1,1 +1,64 @@
+using Microsoft.AspNetCore.Http;
 
+namespace GameVault.BLL.Helpers.UploadFile
+{
+    public static class Upload
+    {
+        public static string UploadFile(string FolderName, IFormFile File)
+        {
+
+            try
+            {
+                //catch the folder Path and the file name in server
+                // 1 ) Get Directory
+                string FolderPath = Directory.GetCurrentDirectory() + "/wwwroot/" + FolderName;
+
+
+                //2) Get File Name
+                string FileName = Guid.NewGuid() + Path.GetFileName(File.FileName);
+                //Guid => Word contain from 36 character
+
+                // 3) Merge Path with File Name
+                string FinalPath = Path.Combine(FolderPath, FileName);
+                //combine put /
+
+                //4) Save File As Streams "Data Overtime"
+                using (var Stream = new FileStream(FinalPath, FileMode.Create))
+                {
+                    File.CopyTo(Stream);
+                }
+
+                return FileName;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+
+        }
+
+
+        public static string RemoveFile(string FolderName, string fileName)
+        {
+
+            try
+            {
+                var directory = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Files", FolderName, fileName);
+
+                if (File.Exists(directory))
+                {
+                    File.Delete(directory);
+                    return "File Deleted";
+                }
+
+                return "File Not Deleted";
+
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+    }
+}
