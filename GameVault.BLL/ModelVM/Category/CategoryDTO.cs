ef0d03160cdbf0b646e7e7779e1dcb13889852eb
bassam348 <162562515+bassam348@users.noreply.github.com>
2025-08-08 23:44:37 +0200

    Update CategoryDTO.cs

--- GameVault.BLL/ModelVM/Category/CategoryDTO.cs
+++ GameVault.BLL/ModelVM/Category/CategoryDTO.cs
@@ -2,10 +2,12 @@ namespace GameVault.BLL.ModelVM.Category
 {
     public class CategoryDTO
     {
-        public int Category_Id { get;  set; }
-        public string Category_Name { get;  set; }
-        public string Description { get;  set; }
-        public DateTime CreatedOn { get;  set; }
+        public int Category_Id { get; set; }
+        public string Category_Name { get; set; }
+        public string Description { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        
 
     }
 }
