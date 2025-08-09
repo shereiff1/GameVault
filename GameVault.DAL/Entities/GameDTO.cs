8a52d2e71fb0231a8b845a19f8c8e6917151e7ec
shereiff1 <147086400+shereiff1@users.noreply.github.com>
2025-08-09 19:56:50 +0300

    Adding a list of reviews and gategories

--- GameVault.DAL/Entities/GameDTO.cs
+++ GameVault.DAL/Entities/GameDTO.cs
@@ -8,8 +8,8 @@ namespace GameVault.DAL.Entities
         public string Title { get; set; }
         public string Description { get; set; }
         public string CompanyName { get; set; }
-        public List<string>? Reviews { get; set; }
-        public List<string>? Categories { get; set; }
+        public List<Review>? Reviews { get; set; }
+        public List<Category>? Categories { get; set; }
         public float Rating { get; set; }
         public string ImagePath { get; set; }
         public decimal Price { get; set; }
