70b7ea6e1132031569124dccfabc1bd24d95a773
bassam348 <162562515+bassam348@users.noreply.github.com>
2025-08-10 00:22:12 +0200

    Update IReviewServices.cs

--- GameVault.BLL/Services/Abstraction/IReviewServices.cs
+++ GameVault.BLL/Services/Abstraction/IReviewServices.cs
@@ -5,7 +5,7 @@ namespace GameVault.BLL.Services.Abstraction
     public interface IReviewServices
     {
         Task<(bool, string?)> CreateAsync(CreateReview review);
-        Task<(bool, string?)> UpdateAsync(UpdateReview review);
+        Task<(bool, string?)> UpdateAsync(ReviewDTO review);
         Task<(bool, string?)> DeleteAsync(int id);
         Task<(bool, List<ReviewDTO>?)> GetAllAsync();
         Task<(bool, ReviewDTO?)> GetByIdAsync(int id);
