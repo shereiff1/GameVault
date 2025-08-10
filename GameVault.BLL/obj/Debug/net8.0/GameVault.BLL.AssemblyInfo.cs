eaf48a02b85a450f438af371b29b05df541e1851
Ibrahim-Abohola <ibrahimbakr2005@gmail.com>
2025-08-10 12:03:15 +0300

    Remove obj folder from repo

--- GameVault.BLL/obj/Debug/net8.0/GameVault.BLL.AssemblyInfo.cs
+++ GameVault.BLL/obj/Debug/net8.0/GameVault.BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameVault.BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b94c3a64d7fe1623c12e665fdc090999325cfec3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e747a07dc469db2635ab66b2d7e0f8887a50037")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameVault.BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameVault.BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
