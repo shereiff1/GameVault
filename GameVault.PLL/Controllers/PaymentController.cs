7402db1fbf8d7a534f26d33890406d78e4b9dfef
shereiff1 <147086400+shereiff1@users.noreply.github.com>
2025-08-11 15:26:25 +0300

    adding payment

---
+++ GameVault.PLL/Controllers/PaymentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GameVault.PLL.Controllers
+{
+    public class PaymentController : Controller
+    {
+
+        public IActionResult BuyGame(int GameId)
+        {
+            ViewBag.GameId = GameId;
+            return View();
+        }
+    }
+
+}
